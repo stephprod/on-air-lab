801394c0fc43c3667815b1b260f7686bd3822ac7
stephane <oneil971@gmail.com>
2018-10-13 17:21:03 +0200

    add files

---
+++ route/chat.js
@@ -0,0 +1,27 @@
+let express = require('express')
+let User = require('../models/req_user')
+let session = require('express-session');
+let router = express.Router()
+
+router.route('/chat')
+	.get((request, response) => {
+		//if (request.session.userId == undefined) {
+		//	response.render('pages/index', {name: "index"})
+		//}else{
+			response.locals.session = request.session
+			response.render('pages/chat2')
+		//}
+		console.log("ID du GARS "+request.session.userId)
+        console.log("NOM du GARS "+request.session.userName)
+	})
+	.post((request, response) => {
+		console.log(request.body);
+		let table = []
+		for (let prop in request.body){
+			table.push(request.body[prop])
+		}
+		request.session.id_coresp = table[2]
+		request.session.coresp_type = table[3]
+		response.end();
+})
+module.exports = router
