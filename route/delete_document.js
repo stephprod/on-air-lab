45aa5aeccc2c8ef7cf0e6e8022ce00517b5598d8
yannick <yannick.elusue@hotmail.fr>
2018-10-13 15:25:53 +0200

    mad-version

---
+++ route/delete_document.js
@@ -0,0 +1,26 @@
+let express = require('express')
+let User = require('../models/req_user')
+let fs = require('fs');
+//let fileUpload = require('express-fileupload')
+let router = express.Router()
+
+router.route('/delete-files')
+	.get((request, response) => {
+		
+	})
+	.post((request, response) => {
+		let table = [], ret = {}
+		console.log(request.body)
+		table.push(request.body.file_profileId)
+		User.delete_document(table, (res) => {
+			if (res > 0){
+				ret.success = true
+			}
+			else
+			{
+				ret.success = false
+			}
+			response.send(ret)
+		})
+	})
+module.exports = router
