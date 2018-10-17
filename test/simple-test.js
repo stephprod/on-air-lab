492c1276598e2a67702ccf010d7f40558d40dd65
yannick <yannick.elusue@hotmail.fr>
2018-10-17 13:42:30 +0200

    Testing connection to the app 3

--- test/simple-test.js
+++ test/simple-test.js
@@ -7,12 +7,13 @@ describe('List of simple mocha tests', function() {
 	this.timeout(15000);
 	before("Require all dependencies", function() {
 		// runs before all tests in this block
-		server.close();
+		/*server.close();
 		server.listen(8080, function (){
 			console.log('listening on :8080 for testing')
 			console.log('host : ');
 			console.log(addresses);
-		});
+		});*/
+		console.log('host : '+addresses[0]);
 	});
 	describe('#indexOf()', function() {
 		it('should return -1 when the value is not present', function(done) {
@@ -22,9 +23,9 @@ describe('List of simple mocha tests', function() {
 	});
 	describe('#test connection to the app on search page', function() {
 		it('should return 200', function(done) {
-			http.get('http://'+addresses[0]+':8080/search', function (res) {
+			http.get('http://'+addresses[0]+':4000/search', function (res) {
 		      	assert.equal(200, res.statusCode);
-    			setTimeout(done, 500);
+		      	done();
 		    });
 		});
 	});
