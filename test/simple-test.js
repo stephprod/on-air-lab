e83f37475feb90e9ae9f4049f0a3e1300b48a6f3
yannick <yannick.elusue@hotmail.fr>
2018-10-17 14:05:30 +0200

    Testing connection to the app 5

--- test/simple-test.js
+++ test/simple-test.js
@@ -3,8 +3,13 @@ var server = require('../server').server;
 var addresses = require('../server').host;
 var app = require('../server').application;
 var http = require('http');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
 describe('List of simple mocha tests', function() {
-	this.timeout(15000);
 	before("Require all dependencies", function() {
 		// runs before all tests in this block
 		/*server.close();
@@ -20,14 +25,19 @@ describe('List of simple mocha tests', function() {
 		  assert.equal([1,2,3].indexOf(4), -1);
 		  done();
 		});
-	});
-	describe('#test connection to the app on search page', function() {
 		it('should return 200', function() {
-			http.get('http://'+addresses[0]+':4000/search', function (res) {
+			/*http.get('http://'+addresses[0]+':4000/search', function (res) {
 		      	assert.equal(200, res.statusCode);
-		    });
+		    });*/
+		    chai.request(server)
+			    .get('http://'+addresses[0]+':4000/search')
+			    .end(function(err, res){
+			      res.should.have.status(200);
+			      done();
+			});
 		});
 	});
+
 	after(function() {
 	    // runs after all tests in this block
 	    server.close();
