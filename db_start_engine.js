d24fc54d73c2cbf12f0308cfcff4c16cf46de065
stephane <oneil971@gmail.com>
2018-10-24 14:53:38 +0200

    cleanning src

--- db_start_engine.js
+++ db_start_engine.js
@@ -4,8 +4,8 @@ let connection = mysql.createConnection({
 	host	 : '127.0.0.1',
 	port	 : '3306',
 	user	 : 'root',
-	password : '',
-	database : 'circleci'
+	password : 'administrateur',
+	database : 'starlife'
 })
 
 module.exports = connection
