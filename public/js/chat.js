b7817e163de6e1e825e937ad3ebaac92c8c3ab57
yannick <yannick.elusue@hotmail.fr>
2018-11-07 21:03:10 +0100

    other

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,97 +2,97 @@ import {update_front_with_msg, update_front_with_errors, update_front_with_succe
 /*import {get_events} from './events.js';
 import {socket, switchRoom} from './socket_modules.js';*/
 $(document).on("click", ".friend", function(){
-var childOffset = $(this).offset();
-var parentOffset = $(this).parent().parent().offset();
-var childTop = childOffset.top - parentOffset.top;
-var clone = $(this).find('img').eq(0).clone();
-var top = childTop+12+"px";
+  var childOffset = $(this).offset();
+  var parentOffset = $(this).parent().parent().offset();
+  var childTop = childOffset.top - parentOffset.top;
+  var clone = $(this).find('img').eq(0).clone();
+  var top = childTop+12+"px";
 
-$(clone).css({'top': top}).addClass("floatingImg").appendTo("#chatbox");
+  $(clone).css({'top': top}).addClass("floatingImg").appendTo("#chatbox");
 
-setTimeout(function(){$("#profile p").addClass("animate");$("#profile").addClass("animate");}, 100);
-setTimeout(function(){
-  $("#chat-messages").addClass("animate");
-  $('.cx, .cy').addClass('s1');
-  setTimeout(function(){$('.cx, .cy').addClass('s2');}, 100);
-  setTimeout(function(){$('.cx, .cy').addClass('s3');}, 200);
-}, 150);
-
-$('.floatingImg').animate({
-  'width': "68px",
-  'left':'108px',
-  'top':'20px'
-}, 200);
-
-var name = $(this).find("p strong").html();
-var email = $(this).find("p span").html();
-$("#profile p").html(name);
-$("#profile span").html(email);
-
-$(".message").not(".right").find("img").attr("src", $(clone).attr("src"));
-$('#friendslist').fadeOut();
-$('#chatview').fadeIn();
-
-$('#close').unbind("click").click(function(){
-  $("#chat-messages, #profile, #profile p").removeClass("animate");
-  $('.cx, .cy').removeClass("s1 s2 s3");
-  $('.floatingImg').animate({
-    'width': "40px",
-    'top':top,
-    'left': '12px'
-  }, 200, function(){$('.floatingImg').remove()});
-
+  setTimeout(function(){$("#profile p").addClass("animate");$("#profile").addClass("animate");}, 100);
   setTimeout(function(){
-    $('#chatview').fadeOut();
-    $('#friendslist').fadeIn();
-  }, 50);
-});
-if ($(this).data('coresp-type') == "4"){
-  $($(".box-module .module")[0]).hide();
-  $($(".box-module .module")[2]).show();
-  $($(".box-module .module")[3]).hide();
-  $($(".box-module .module")[1]).show();
-  $($(".box-module .module")[4]).show();
-  $($(".box-module .module")[5]).show();
-  $("#devis-modal").find("h1").empty();
-  $("#devis-modal").find("h1").append("Envoyer un devis");
-}
-else if($(this).data('coresp-type') == "1"){
-  $($(".box-module .module")[0]).hide();
-  $($(".box-module .module")[1]).hide();
-  $($(".box-module .module")[2]).hide();
-  $($(".box-module .module")[3]).hide();
-  $($(".box-module .module")[4]).hide();
-  $($(".box-module .module")[5]).hide();
-}
-else if ($(this).data('coresp-type') == "2"){
-  $("select[name='services']").remove();
-  $('#devis-modal').find('[name="msg"]').after('<select name="services" class="bootstrap-select" title="Services:" multiple data-actions-box="true"></select>');
-  $($(".box-module .module")[0]).show();
-  $($(".box-module .module")[1]).show();
-  $($(".box-module .module")[2]).show();
-  $($(".box-module .module")[3]).show();
-  $($(".box-module .module")[4]).show();
-  $($(".box-module .module")[5]).show();
-  $("#devis-modal").find("h1").empty();
-  $("#devis-modal").find("h1").append("Demande de devis");
-  $('#devis-modal').find("a#devis-send span").empty();
-  $('#devis-modal').find("a#devis-send span").append("Envoyer la demande");
-}else{
-  $("select[name='services']").remove();
-  $('#devis-modal').find('[name="msg"]').after('<select name="services" class="bootstrap-select" title="Services:" multiple data-actions-box="true"></select>');
-  $($(".box-module .module")[0]).hide();
-  $($(".box-module .module")[1]).show();
-  $($(".box-module .module")[2]).show();
-  $($(".box-module .module")[3]).show();
-  $($(".box-module .module")[4]).show();
-  $($(".box-module .module")[5]).show();
-  $("#devis-modal").find("h1").empty();
-  $("#devis-modal").find("h1").append("Demande de devis");
-  $('#devis-modal').find("a#devis-send span").empty();
-  $('#devis-modal').find("a#devis-send span").append("Envoyer la demande");
-}
-switchRoom($(this).data("room"), $(this).data("coresp"), name.split(" ")[0], name.split(" ")[1], $(this).data('coresp-type'), $(this).data('coresp-payment'), user);
+    $("#chat-messages").addClass("animate");
+    $('.cx, .cy').addClass('s1');
+    setTimeout(function(){$('.cx, .cy').addClass('s2');}, 100);
+    setTimeout(function(){$('.cx, .cy').addClass('s3');}, 200);
+  }, 150);
+
+  $('.floatingImg').animate({
+    'width': "68px",
+    'left':'108px',
+    'top':'20px'
+  }, 200);
+
+  var name = $(this).find("p strong").html();
+  var email = $(this).find("p span").html();
+  $("#profile p").html(name);
+  $("#profile span").html(email);
+
+  $(".message").not(".right").find("img").attr("src", $(clone).attr("src"));
+  $('#friendslist').fadeOut();
+  $('#chatview').fadeIn();
+
+  $('#close').unbind("click").click(function(){
+    $("#chat-messages, #profile, #profile p").removeClass("animate");
+    $('.cx, .cy').removeClass("s1 s2 s3");
+    $('.floatingImg').animate({
+      'width': "40px",
+      'top':top,
+      'left': '12px'
+    }, 200, function(){$('.floatingImg').remove()});
+
+    setTimeout(function(){
+      $('#chatview').fadeOut();
+      $('#friendslist').fadeIn();
+    }, 50);
+  });
+  if ($(this).data('coresp-type') == "4"){
+    $($(".box-module .module")[0]).hide();
+    $($(".box-module .module")[2]).show();
+    $($(".box-module .module")[3]).hide();
+    $($(".box-module .module")[1]).show();
+    $($(".box-module .module")[4]).show();
+    $($(".box-module .module")[5]).show();
+    $("#devis-modal").find("h1").empty();
+    $("#devis-modal").find("h1").append("Envoyer un devis");
+  }
+  else if($(this).data('coresp-type') == "1"){
+    $($(".box-module .module")[0]).hide();
+    $($(".box-module .module")[1]).hide();
+    $($(".box-module .module")[2]).hide();
+    $($(".box-module .module")[3]).hide();
+    $($(".box-module .module")[4]).hide();
+    $($(".box-module .module")[5]).hide();
+  }
+  else if ($(this).data('coresp-type') == "2"){
+    $("select[name='services']").remove();
+    $('#devis-modal').find('[name="msg"]').after('<select name="services" class="bootstrap-select" title="Services:" multiple data-actions-box="true"></select>');
+    $($(".box-module .module")[0]).show();
+    $($(".box-module .module")[1]).show();
+    $($(".box-module .module")[2]).show();
+    $($(".box-module .module")[3]).show();
+    $($(".box-module .module")[4]).show();
+    $($(".box-module .module")[5]).show();
+    $("#devis-modal").find("h1").empty();
+    $("#devis-modal").find("h1").append("Demande de devis");
+    $('#devis-modal').find("a#devis-send span").empty();
+    $('#devis-modal').find("a#devis-send span").append("Envoyer la demande");
+  }else{
+    $("select[name='services']").remove();
+    $('#devis-modal').find('[name="msg"]').after('<select name="services" class="bootstrap-select" title="Services:" multiple data-actions-box="true"></select>');
+    $($(".box-module .module")[0]).hide();
+    $($(".box-module .module")[1]).show();
+    $($(".box-module .module")[2]).show();
+    $($(".box-module .module")[3]).show();
+    $($(".box-module .module")[4]).show();
+    $($(".box-module .module")[5]).show();
+    $("#devis-modal").find("h1").empty();
+    $("#devis-modal").find("h1").append("Demande de devis");
+    $('#devis-modal').find("a#devis-send span").empty();
+    $('#devis-modal').find("a#devis-send span").append("Envoyer la demande");
+  }
+  switchRoom($(this).data("room"), $(this).data("coresp"), name.split(" ")[0], name.split(" ")[1], $(this).data('coresp-type'), $(this).data('coresp-payment'), user);
 });
 function on_socket_update_rooms(rooms, current_room, coresp){
 //console.log("updateroom");
